10f6b825e62ffb93805b3fc2e4cf81ac3b736943
dludwin <48782906+dludwin@users.noreply.github.com>
2020-10-12 16:44:14 +0200

    commit

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Container, Inner} from './styles/jumbotron';
 
-export default function Jumbotron({ children, direction = 'row', ...restProps }) {          // user can pass anything. If you pass object it will apply to the first that comes in 
+export default function Jumbotron( { children, direction = 'row', ...restProps } ) {          // user can pass anything. If you pass object it will apply to the first that comes in 
     return (
     <Inner direction={direction}>
         <p> Hello again! </p>
--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components/macro';  // macro will give the name of the component in the dev tools. Instead of some random number 
 
 export const Inner = styled.div`     /*  h1 div or else */
-    display: flex;
-    align-items: center;
+    display: flex;                
+    align-items: center;        
     flex-direction: ${({ direction }) => direction};
     justify-content: space-between;
     max-width: 1100px;
