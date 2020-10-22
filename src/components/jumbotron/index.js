a560ac1288e876ff39ad6fc5ecad55a2dad18492
dludwin <48782906+dludwin@users.noreply.github.com>
2020-10-22 22:06:02 +0200

    commit

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,36 +1,43 @@
 import React from 'react';
-import {Container, Inner, Item, Title, SubTitle, Pane, Image} from './styles/jumbotron';
+import {
+	Container,
+	Inner,
+	Item,
+	Title,
+	SubTitle,
+	Pane,
+	Image,
+} from './styles/jumbotron';
 
-export default function Jumbotron( { children, direction = 'row', ...restProps } ) {          // children always come first, user can pass anything. If you pass object it will apply to the first that comes in 
-    return (
-    
-        <Item {...restProps}>                            
-           <Inner direction={direction}>
-                {children} 
-           </Inner>                                                                                 {/* This is what encapsulates compound components */}
-        </Item>
-    )
+export default function Jumbotron({
+	children,
+	direction = 'row',
+	...restProps
+}) {
+	// children always come first, user can pass anything. If you pass object it will apply to the first that comes in
+	return (
+		<Item {...restProps}>
+			<Inner direction={direction}>{children}</Inner>
+		</Item>
+	);
 }
 
-Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
-return <Container {...restProps}> {children} </Container>; 
-}
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+	return <Container {...restProps}> {children} </Container>;
+};
 
-Jumbotron.Pane = function JumbotronPane({children, ...restProps}) {
-    return <Pane {...restProps}> {children} </Pane>; 
-    }
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+	return <Pane {...restProps}> {children} </Pane>;
+};
 
-    /* with compound components it gets very repetitive so we can copy and paste */
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+	return <Title {...restProps}> {children} </Title>;
+};
 
-Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
-    return <Title {...restProps}> {children} </Title>; 
-    }
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+	return <SubTitle {...restProps}> {children} </SubTitle>;
+};
 
-Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}) {
-    return <SubTitle {...restProps}> {children} </SubTitle>; 
-    }
-
-Jumbotron.Image = function JumbotronImage({...restProps}) {
-    return <Image {...restProps} />; 
-    }
-
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+	return <Image {...restProps} />;
+};
--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,55 +1,57 @@
-import styled from 'styled-components/macro';                        // macro will give the name of the component in the dev tools. Instead of some random number 
+import styled from 'styled-components/macro'; // macro will give the name of the component in the dev tools. Instead of some random number
 
-export const Item = styled.div`                 
-    display: flex;
-    border-bottom: 8px solid #222;
-    padding: 50px 5%;
-    color: white;
-    overflow: hidden;
+export const Item = styled.div`
+	display: flex;
+	border-bottom: 8px solid #222;
+	padding: 50px 5%;
+	color: white;
+	overflow: hidden;
 `;
 
-export const Inner = styled.div`                                     /*  h1 div or else */
-    display: flex;                
-    align-items: center;        
-    flex-direction: ${ ({ direction }) => direction };
-    justify-content: space-between;
-    max-width: 1100px;
-    margin: auto;                           /* that makes marhin of Inner automatically 56 0 56 0 */
-    width: 100%;
+export const Inner = styled.div`
+	/*  h1 div or else */
+	display: flex;
+	align-items: center;
+	flex-direction: ${({ direction }) => direction};
+	justify-content: space-between;
+	max-width: 1100px;
+	margin: auto; /* that makes marhin of Inner automatically 56 0 56 0 */
+	width: 100%;
 
-    @media (max-width: 1000px) {             /* when browser window is 1000px or less direction of content will be column */
-        flex-direction: column;
-    }
+	@media (max-width: 1000px) {
+		/* when browser window is 1000px or less direction of content will be column */
+		flex-direction: column;
+	}
 `;
 
 export const Pane = styled.div`
-    width: 50%;
+	width: 50%;
 
-    @media (max-width: 1000px) {
-        width: 100%;
-        padding: 0 45px;
-        text-align: center;
-    }
+	@media (max-width: 1000px) {
+		width: 100%;
+		padding: 0 45px;
+		text-align: center;
+	}
 `;
 
-export const Title = styled.h1`  
-    font-size: 50px;
-    line-height: 1.1;
-    margin-bottom: 8px;
+export const Title = styled.h1`
+	font-size: 50px;
+	line-height: 1.1;
+	margin-bottom: 8px;
 
-    @media (max-width: 600px) {
-        font-size: 35px;
-    }
+	@media (max-width: 600px) {
+		font-size: 35px;
+	}
 `;
 
 export const SubTitle = styled.h2`
-    font-size: 32px;
-    font-weight: bold;
-    line-height: normal;
+	font-size: 32px;
+	font-weight: bold;
+	line-height: normal;
 
-    @media (max-width: 600px){
-        font-size: 18px;
-    }
+	@media (max-width: 600px) {
+		font-size: 18px;
+	}
 `;
 
 export const Image = styled.img`
@@ -58,9 +60,9 @@ export const Image = styled.img`
 `;
 
 export const Container = styled.div`
-    @media (max-width: 1000px ){
-        ${Item}: last-of-type h2 {
-            margin-bottom: 50px;
-        }
-    }
-`;                                      
+	@media (max-width: 1000px) {
+		${Item}: last-of-type h2 {
+			margin-bottom: 50px;
+		}
+	}
+`;
