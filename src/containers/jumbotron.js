78a212b47df9ad9bd78fc28eca0a283e0bb985af
dludwin <48782906+dludwin@users.noreply.github.com>
2020-10-22 22:54:19 +0200

    commit

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import {Jumbotron} from '../components';
+import { Jumbotron } from '../components';
 import jumboData from '../fixtures/jumbo.json';
 
 export function JumbotronContainer() {
-  return (
-    <Jumbotron.Container>    {/* encapsule all the 3 jumbotrons from jumbo.json */}
-    
-      {jumboData.map((item) => (            // for each item that comes down from the map, we can iterate 
-       
-        <Jumbotron key={item.id} direction = {item.direction}>                {/*  // need to pass key to iteration element */}           
-           
-            <Jumbotron.Pane> 
-                <Jumbotron.Title> {item.title}</Jumbotron.Title>
-                <Jumbotron.SubTitle> {item.subTitle} </Jumbotron.SubTitle>
-            </Jumbotron.Pane> 
-
-            <Jumbotron.Pane>                                              {/* right side, and we will just flip with flex */}
-                <Jumbotron.Image  src={item.image} alt={item.alt} />
-            </Jumbotron.Pane> 
-       
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+	return (
+		<Jumbotron.Container>
+			{' '}
+			{/* encapsule all the 3 jumbotrons from jumbo.json */}
+			{jumboData.map((
+				item // for each item that comes down from the map, we can iterate
+			) => (
+				<Jumbotron key={item.id} direction={item.direction}>
+					{' '}
+					{/*  // need to pass key to iteration element */}
+					<Jumbotron.Pane>
+						<Jumbotron.Title> {item.title}</Jumbotron.Title>
+						<Jumbotron.SubTitle> {item.subTitle} </Jumbotron.SubTitle>
+					</Jumbotron.Pane>
+					<Jumbotron.Pane>
+						{' '}
+						{/* right side, and we will just flip with flex */}
+						<Jumbotron.Image src={item.image} alt={item.alt} />
+					</Jumbotron.Pane>
+				</Jumbotron>
+			))}
+		</Jumbotron.Container>
+	);
 }
