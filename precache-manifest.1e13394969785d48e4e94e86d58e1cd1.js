5b2bf2b3067d608fff729f0594823865e117094a
dludwin <48782906+dludwin@users.noreply.github.com>
2020-10-22 22:59:03 +0200

    Updates

---
+++ precache-manifest.1e13394969785d48e4e94e86d58e1cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d4d483a6af6842460e0d848afa3839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053217fe77dbb226f92f",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "053217fe77dbb226f92f",
+    "url": "/static/js/2.d194a4d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d194a4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b78e5e40faa7b3ed87",
+    "url": "/static/js/main.9e76aadb.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  }
+]);
