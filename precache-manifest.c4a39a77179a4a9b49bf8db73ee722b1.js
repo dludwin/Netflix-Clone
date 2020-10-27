4e2c5555ebd49cf5f1e14e6b27edcf2e05f6f4f8
dludwin <48782906+dludwin@users.noreply.github.com>
2020-10-27 09:50:02 +0100

    Updates

---
+++ precache-manifest.c4a39a77179a4a9b49bf8db73ee722b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b255f7a9e8cfa61ccbf798fc923298",
+    "url": "/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "e42f70392b9e6684bffd",
+    "url": "/Netflix-Clone/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e42f70392b9e6684bffd",
+    "url": "/Netflix-Clone/static/js/2.184c715e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Netflix-Clone/static/js/2.184c715e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b78e5e40faa7b3ed87",
+    "url": "/Netflix-Clone/static/js/main.9e76aadb.chunk.js"
+  },
+  {
+    "revision": "ef6320a25b082e7d12a9",
+    "url": "/Netflix-Clone/static/js/runtime-main.fa37907a.js"
+  }
+]);
