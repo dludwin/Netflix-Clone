22a105f9b5a41c10a9e6659527f610ab8b2e8d8d
dludwin <48782906+dludwin@users.noreply.github.com>
2021-04-09 10:02:15 +0200

    commit

--- src/containers/header.js
+++ src/containers/header.js
@@ -4,17 +4,15 @@ import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 export function HeaderContainer({ children }) {
-	// HeaderContainer is container for the Header and returns only Header
+  // HeaderContainer is container for the Header and returns only Header
 
-	return (
-		<Header>
-			<Header.Frame>
-				<Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-				<Header.ButtonLink to={ROUTES.SIGN_IN}>Sign in</Header.ButtonLink>
-			</Header.Frame>
-			{children}
-		</Header>
-	);
+  return (
+    <Header>
+      <Header.Frame>
+        <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+      </Header.Frame>
+      {children}
+    </Header>
+  );
 }
-
-
